59838a6a3159298f9504f6d3522f7bfb9bf2d3a1
thebinary <binary4bytes@gmail.com>
2019-09-30 17:56:23 +0545

    return result type as given in doc

--- builder/object.go
+++ builder/object.go
@@ -11,15 +11,15 @@ const template_obj = `// This is a generated file. DO NOT EDIT manually.
 
 package {{.Package}}
 {{$o := .Object}}
-//{{CamelCase .Object.Name}}Record: {{.Object.Description}}
-type {{CamelCase .Object.Name}}Record struct {
+//{{CamelCase .Object.Name}}: {{.Object.Description}}
+type {{CamelCase .Object.Name}} struct {
   {{range .Object.Fields}}{{CamelCase .Name}} {{.Type}}               // {{.Description}}
   {{end}}
 }
 
 {{range .Object.Messages}}
-/* {{CamelCase .Name}}: {{.Description}} */
-func (client *XenClient) {{CamelCase $o.Name}}{{CamelCase .Name}}({{range .Params}}{{if (eq .Name "session_id")}}{{else}}{{if (eq .Name "type")}}xtype{{else}}{{.Name}}{{end}} {{TypeName .Type}},{{end}}{{end}}) (result xmlrpc.Struct, err error){
+/* {{CamelCase .Name}}: {{.Description}} */{{$resultType := index .Result 0}}
+func (client *XenClient) {{CamelCase $o.Name}}{{CamelCase .Name}}({{range .Params}}{{if (eq .Name "session_id")}}{{else}}{{if (eq .Name "type")}}xtype{{else}}{{.Name}}{{end}} {{TypeName .Type}},{{end}}{{end}}) ({{if (eq $resultType "void")}}{{else}}result {{TypeName $resultType}}, {{end}}err error){
 	log.Println("called {{$o.Name}}.{{.Name}}")
 	return
   //return client.Call("{{$o.Name}}.{{.Name}}", {{range .Params}}{{if (eq .Name "session_id")}}{{else}}{{if (eq .Name "type")}}xtype{{else}}{{.Name}}{{end}},{{end}}{{end}})
